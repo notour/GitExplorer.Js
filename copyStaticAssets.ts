1b2be8fe11322659152dd1d8f9aa5cfc61518e23
Mickael Thumerel <mickael.thumerel@outlook.com>
2018-05-25 14:56:49 +0200

    Setup basic config and environment

---
+++ copyStaticAssets.ts
@@ -0,0 +1,5 @@
+import * as shell from "shelljs";
+
+// shell.cp("-R", "src/client/js/lib", "dist/client/resourses/js/");
+// shell.cp("-R", "src/client/fonts", "dist/client/resourses/");
+// shell.cp("-R", "src/client/images", "dist/client/resourses/");
